8da8c6f2879032d139a2cbc356ab9f7a973014ae
Bananayosostene <sbananayo98@gmail.com>
2025-07-01 18:09:04 +0200

    make app to be searcheble

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,40 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'DSS',
-  description: 'Digital Support Solutions',
-  generator: 'DSS.dev',
-}
+  title: "DSS | 24/7 Digital Support Solutions",
+  description:
+    "Providing 24/7 IT support, remote services, and on-site assistance in Kigali, Rwanda.",
+  keywords: [
+    "IT Support",
+    "DSS",
+    "Digital Support",
+    "Remote Help",
+    "Tech Solutions Kigali",
+  ],
+  authors: [{ name: "DSS Team", url: "https://dss-dev.vercel.app/" }],
+  creator: "DSS.dev",
+  metadataBase: new URL("https://dss-dev.vercel.app/"),
+  openGraph: {
+    title: "DSS | Digital Support Solutions",
+    description: "Get reliable 24/7 remote and on-site IT support.",
+    url: "https://dss-dev.vercel.app/",
+    siteName: "DSS",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  other: {
+    "google-site-verification": "Z8Zyh_IB34EXGkUIrbpPqFnF4gLwm3S80-TscqYCSMQ",
+  },
+};
+
+
 
 export default function RootLayout({
   children,
